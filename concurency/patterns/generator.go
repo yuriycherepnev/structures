e32586e9f090df9be3ba8b7878699b97d9358adc
yuriy <yuriy.cherepnev@gmail.com>
2026-06-24 12:48:12 +0300

    111

---
+++ concurency/patterns/generator.go
@@ -0,0 +1,29 @@
+// Generator - микропаттерн, который наполняет канал. Закрываем канал, чтобы не было проблем.
+// паттерн генератор не должен содержать в себе блокирующих операций
+
+package main
+
+import (
+	"fmt"
+)
+
+func generator() <-chan int {
+	ch := make(chan int)
+
+	go func() {
+		for i := 0; i <= 10; i++ {
+			ch <- i + 1
+		}
+		close(ch)
+	}()
+
+	return ch
+}
+
+func main() {
+	ch := generator()
+
+	for result := range ch {
+		fmt.Println(result)
+	}
+}
